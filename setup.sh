0bfff7bb2d5d5a7f5b7105669002439e2273905a
Jourdan Rodrigues <thiagojourdan@gmail.com>
2022-08-24 11:01:49 -0300

    :sparkles: Also install NVM and Node LTS during setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-# Install "Oh my ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#!/bin/bash
 
+# Install "Oh my ZSH"
+bash <(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+
+# Install NVM (Node Version Manager)
+bash <(curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh)
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# General setup
+nvm install --lts
 git config --global init.defaultBranch main
